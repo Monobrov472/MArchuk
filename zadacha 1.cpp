e668c64a9c4534c494b1f0cc85495abcd5909b11
Monobrov472 <144647756+Monobrov472@users.noreply.github.com>
2023-09-24 17:46:33 +0300

    Add files via upload

---
+++ zadacha 1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+
+    // Введення значень A, B і C
+    cout << "Enter value A: ";
+    cin >> A;
+    cout << "Enter value B: ";
+    cin >> B;
+    cout << "Enter value C: ";
+    cin >> C;
+
+    // Обмін значень
+    int temp = A;
+    A = B;
+    B = C;
+    C = temp;
+
+    // Виведення нових значень
+    cout << "New value A: " << A << endl;
+    cout << "New value  B: " << B << endl;
+    cout << "New value  C: " << C << endl;
+
+    return 0;
+}
